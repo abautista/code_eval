1ed355970077d323ff6dd0797c3e2c554e082dc1
Alberto Bautista <alberto.bautista@me.com>
2015-04-04 10:29:38 +0200

    Added word to digit and cash register challenges.

---
+++ challenges/104_word_to_digit/word_to_digit.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+d = {
+  "zero" => "0",
+  "one" => "1",
+  "two" =>"2",
+  "three" => "3",
+  "four" => "4",
+  "five" => "5",
+  "six" => "6",
+  "seven" => "7",
+  "eight" => "8",
+  "nine" => "9",
+}
+
+File.open(ARGV[0]).each_line do |line|
+  puts line.chop.split(";").map { |x| d[x] }.join
+end
