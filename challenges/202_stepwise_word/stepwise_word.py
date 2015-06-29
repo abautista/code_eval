56ee74dce35c0d66081a14ee4b30d291982d95ab
Alberto Bautista <alberto.bautista@me.com>
2015-06-29 21:42:01 +0200

    Added stepwise word challenge.

---
+++ challenges/202_stepwise_word/stepwise_word.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def make_stepwise(s):
+    max_len_index = 0
+    for index in range(0, len(s)):
+        if len(s[index]) > len(s[max_len_index]):
+            max_len_index = index
+
+    result = ""
+    for i, item in enumerate(s[max_len_index]):
+        if i == 0:
+            result += item
+        else:
+            result = result + " " + "*" * i + item
+    return result
+
+if __name__ == '__main__':
+
+    print make_stepwise(["cat", "dog", "hello"])
+    with open(sys.argv[1], "r") as test_cases:
+        for test in test_cases:
+            s = test.replace("\n", "").split(" ")
+            print make_stepwise(s)
